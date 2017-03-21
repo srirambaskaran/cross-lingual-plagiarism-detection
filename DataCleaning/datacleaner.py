266b3c2c93e9ba995b609f342eded61bffd1bd51
Sriram Baskaran <sbaskara@usc.edu>
2017-03-20 19:41:52 -0700

    Cleaned sample 10000 parallel corpus
    Cleaned the sample data set I downloaded. It contains tab separated
    sentences. First column in English and second column in German.
    
    Added the data cleaner code that did the task.

--- DataCleaning/datacleaner.py
+++ DataCleaning/datacleaner.py
@@ -1,5 +1,24 @@
 import xml.etree.ElementTree
+import codecs
 
-e = xml.etree.ElementTree.parse('../webcrawl-deen-sample.tmx').getroot()
+root = xml.etree.ElementTree.parse('webcrawl-deen-sample.tmx').getroot()
 
+tuTags = root.findall("body/tu")
 
+f = codecs.open("parallel_10000_sample.txt", "w", "utf-8")
+for tu in tuTags:
+    print(tu.attrib["tuid"])
+    tuvs = tu.findall("tuv")
+    en_sentence = ""
+    de_sentence = ""
+
+    for tuv in tuvs:
+        lang = tuv.attrib["lang"]
+        if lang == "en":
+            en_sentence = tuv[2].text
+        else:
+            de_sentence = tuv[2].text
+
+    f.write(en_sentence+"\t"+de_sentence+"\n")
+
+f.close()
