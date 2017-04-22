33d5dd8150b00946fe9eb514d73661bdb0375b8c
Sriram Baskaran <sbaskara@usc.edu>
2017-04-21 19:40:28 -0700

    Changing to Sum. Using LinReg and Cross Validation

--- CLPD-Code/sequence_embedding.py
+++ CLPD-Code/sequence_embedding.py
@@ -6,6 +6,10 @@ import ConfigParser
 import numpy as np
 from scipy import spatial
 import cPickle as pickle
+import sklearn as sk
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split, cross_val_score, ShuffleSplit
+from sklearn.metrics import r2_score
 
 def read_config(file, section):
 	Config = ConfigParser.ConfigParser()
@@ -28,13 +32,12 @@ for line in file:
 
 english_embedding = pickle.load(open(configs['english_embeddings'],"rb"))
 foreign_embedding = pickle.load(open(configs['foreign_embeddings'],"rb"))
-similarity = 0.0
-similarities = []
-np.seterr(all='raise')
-total = 0
+
+english = []
+foreign = []
 for i in range(0, len(foreign_lines)):
-	foreign_words = foreign_lines[i].split(" ")
-	english_words = english_lines[i].split(" ")
+	foreign_words = foreign_lines[i].split()
+	english_words = english_lines[i].split()
 
 	english_average = np.full((64), 0.0)
 	english_total = 0
@@ -50,20 +53,29 @@ for i in range(0, len(foreign_lines)):
 			foreign_average += foreign_embedding[word]
 			foreign_total+=1
 	
-	if foreign_total == 0:
-		continue
-	if english_total == 0:
-		continue
-	english_average = np.divide(english_average, english_total)
-	foreign_average = np.divide(foreign_average, foreign_total)
+	# if foreign_total == 0:
+	# 	continue
+	# if english_total == 0:
+	# 	continue
+	# english_average = np.divide(english_average, english_total)
+	# foreign_average = np.divide(foreign_average, foreign_total)
+	english.append(english_average)
+	foreign.append(foreign_average)
 
-	
-	if foreign_total !=0 and english_total != 0:
-		total+=1
-		similarity = np.dot(english_average, foreign_average) / (np.linalg.norm(english_average) * np.linalg.norm(foreign_average))
-		similarities.append(similarity)
+print "Fitting a linear regression model"
+regression = linear_model.LinearRegression()
+# english_train, english_test, foreign_train, foreign_test = train_test_split(english, foreign, test_size=0.2, random_state=0)
+# regression.fit(english_train, foreign_train)
+# pickle.dump(regression,open("linear_mode.pickle","wb"))
 
-print "Maximum Similarity: " + str(max(similarities))
-print "Minimum Similarity: " + str(min(similarities))
-print "Average Similarity: " + str(sum(similarities)/len(similarities))
+scores = cross_val_score(regression, english, foreign, cv=ShuffleSplit(n_splits=10, test_size=0.1, random_state=0))
+print scores
 
+# print('Coefficients: \n'+str(regression.coef_))
+# print regression.predict(english_test[0].reshape(1,-1))
+# print foreign_test[0]
+# v1 = [1,2,3]
+# v2 = [5,2,4]
+# print np.mean((v1 - v2) ** 2)
+# print("Mean squared error: %.2f" % np.mean((regression.predict(english_test) - foreign_test) ** 2))
+# print("R2 Score: %.2f" % r2_score(foreign_test,regression.predict(english_test)))
